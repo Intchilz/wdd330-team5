6fb4a9bd4ed96d9e2161447c29a3a348e2b73e0b
AdongoNat <danieladongonat@gmail.com>
2025-07-05 14:49:14 +0000

    add to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,21 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Retrieve existing cart items from local storage or initialize an empty array
+  let cartItems = getLocalStorage("so-cart") || [];
+
+  // Ensure cartItems is an array
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
+
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
